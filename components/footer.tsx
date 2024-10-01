1fd5b4c26ff184d9630f1e62fb665635e4cfc125
dominik <dominikxmichalik@gmail.com>
2024-10-01 15:32:01 +0200

    [BRAVE-8] - add pages to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,20 +2,20 @@ import Link from 'next/link'
 
 import { GitHubIcon, TwitterIcon } from '@/icons'
 
-function Footer({ categories = [], collections = [] }) {
+function Footer({ categories = [], footerPages = [] }) {
   return (
     <footer className="bg-white" aria-labelledby="footerHeading">
       <h2 id="footerHeading" className="sr-only">
         Footer
       </h2>
-      <div className="mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+      <div className="mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 border-t border-gray-200">
         <div className="pb-8 xl:grid xl:grid-cols-5 xl:gap-8">
           <div className="grid grid-cols-2 gap-8 xl:col-span-4">
             <div className="space-y-12 md:grid md:grid-cols-2 md:gap-8 md:space-y-0">
               {categories.length ? (
                 <div>
                   <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                    Categories
+                    Kategorie
                   </h3>
                   <ul className="mt-4 space-y-4">
                     {categories.map((category) => (
@@ -33,31 +33,31 @@ function Footer({ categories = [], collections = [] }) {
                   </ul>
                 </div>
               ) : null}
-              {collections.length ? (
-                <div>
-                  <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                    Collections
-                  </h3>
-                  <ul className="mt-4 space-y-4">
-                    {collections.map((collection) => (
-                      <li key={collection.id}>
-                        <Link
-                          href={`/${collection.type.toLowerCase()}/${
-                            collection.slug
-                          }`}
-                          className="text-base text-gray-500 hover:text-gray-900"
-                        >
-                          {collection.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+            </div>
+            <div className="space-y-12 md:grid md:grid-cols-2 md:gap-8 md:space-y-0">
+              {footerPages.length ? (
+                  <div>
+                    <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
+                      Pomoc
+                    </h3>
+                    <ul className="mt-4 space-y-4">
+                      {footerPages.map((page) => (
+                          <li key={page.id}>
+                            <Link
+                                href={`/menu/${page.slug}`}
+                                className="text-base text-gray-500 hover:text-gray-900"
+                            >
+                              {page.title}
+                            </Link>
+                          </li>
+                      ))}
+                    </ul>
+                  </div>
               ) : null}
             </div>
           </div>
         </div>
-        <div className="mt-8 border-t border-gray-200 pt-8 md:flex md:items-center md:justify-between">
+        <div className="mt-8 pt-8 md:flex md:items-center md:justify-between">
           <div className="flex space-x-6 md:order-2">
             <Link href="https://twitter.com/hygraphcom" className="text-gray-400 hover:text-gray-500">
               <span className="sr-only">Twitter</span>
---
+++ pages/menu/[slug].js
@@ -0,0 +1,57 @@
+import * as React from 'react'
+
+import getPageData from '@/lib/get-page-data'
+import getFooterPageBySlug from "@/lib/get-footer-page-slug";
+import getAllFooterPages from "@/lib/get-all-footer-pages";
+import SectionTitle from "@/components/ui/typography/sectionTitle";
+import ReactMarkdown from "react-markdown";
+
+function FooterPageData({ footerPageData }) {
+    return (
+        <div className="mt-40 mb-20">
+            <SectionTitle title={footerPageData.title} />
+            <div className="footer-menu max-w-2xl mx-auto">
+                <ReactMarkdown>
+                    {footerPageData.content.markdown}
+                </ReactMarkdown>
+            </div>
+        </div>
+    )
+}
+
+export async function getStaticPaths({ locales }) {
+    let paths = []
+
+    for (const locale of locales) {
+        const { footerPages } = await getAllFooterPages({ locale });
+        paths = [
+            ...paths,
+            ...footerPages.map((footerPage) => ({
+                params: { slug: footerPage.slug },
+                locale
+            }))
+        ]
+    }
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ locale, params }) {
+    const pageData = await getPageData({ locale })
+    const { footerPageData } = await getFooterPageBySlug({
+        locale,
+        slug: params.slug
+    })
+
+    return {
+        props: {
+            footerPageData: footerPageData.footerPages[0],
+            ...pageData
+        }
+    }
+}
+
+export default FooterPageData
